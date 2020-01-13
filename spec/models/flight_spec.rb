a5b17d980476eb44cc93ba6260e4fedd44cfd8f8
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-13 11:24:40 -0700

    Add tets for adult and minor count

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -15,4 +15,46 @@ RSpec.describe Flight, type: :model do
     it {should have_many :flight_passengers}
     it {should have_many(:passengers).through(:flight_passengers)}
   end
+
+  describe "instance methods" do
+    it "can calculate number of minors" do
+      airline_1 = Airline.create(name: "United")
+      flight_1 = Flight.create(number: "B5EX", date: "02/24/2020", time: "1:30pm", arrival_city: "Chicago", departure_city: "Denver", airline_id: airline_1.id)
+
+      passenger_1 = Passenger.create(name: "Caligula", age: 22)
+      passenger_2 = Passenger.create(name: "Nero", age: 17)
+      passenger_3 = Passenger.create(name: "Julius Caeser", age: 22)
+      passenger_4 = Passenger.create(name: "Augustus", age: 16)
+      passenger_5 = Passenger.create(name: "Claudius", age: 25)
+
+      FlightPassenger.create(flight: flight_1, passenger: passenger_1)
+      FlightPassenger.create(flight: flight_1, passenger: passenger_2)
+      FlightPassenger.create(flight: flight_1, passenger: passenger_3)
+      FlightPassenger.create(flight: flight_1, passenger: passenger_4)
+      FlightPassenger.create(flight: flight_1, passenger: passenger_5)
+
+      expect(flight_1.minor_count).to eq([passenger_2, passenger_4])
+      expect(flight_1.minor_count.count).to eq(2)
+    end
+
+    it "can calculate number of adults" do
+      airline_1 = Airline.create(name: "United")
+      flight_1 = Flight.create(number: "B5EX", date: "02/24/2020", time: "1:30pm", arrival_city: "Chicago", departure_city: "Denver", airline_id: airline_1.id)
+
+      passenger_1 = Passenger.create(name: "Caligula", age: 22)
+      passenger_2 = Passenger.create(name: "Nero", age: 17)
+      passenger_3 = Passenger.create(name: "Julius Caeser", age: 22)
+      passenger_4 = Passenger.create(name: "Augustus", age: 16)
+      passenger_5 = Passenger.create(name: "Claudius", age: 25)
+
+      FlightPassenger.create(flight: flight_1, passenger: passenger_1)
+      FlightPassenger.create(flight: flight_1, passenger: passenger_2)
+      FlightPassenger.create(flight: flight_1, passenger: passenger_3)
+      FlightPassenger.create(flight: flight_1, passenger: passenger_4)
+      FlightPassenger.create(flight: flight_1, passenger: passenger_5)
+
+      expect(flight_1.adult_count).to eq([passenger_1, passenger_3, passenger_5])
+      expect(flight_1.adult_count.count).to eq(3)
+    end
+  end
 end
