9c82e79777bb793dd2035cf89b37dfb0f9b52844
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-13 11:00:41 -0700

    Add update method

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,4 +3,11 @@ class PassengersController < ApplicationController
   def show
     @passenger = Passenger.find(params[:id])
   end
+
+  def update
+    passenger = Passenger.find(params[:id])
+    current_flights = passenger.flights.all
+    # require "pry"; binding.pry
+    redirect_to "/passengers/#{passenger.id}"
+  end
 end
