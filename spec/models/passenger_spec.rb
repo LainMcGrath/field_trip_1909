5026f7a2ea1e9f19b1b3c96b546039eec4d83497
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-13 10:58:58 -0700

    Add test for adding new flight to passenger

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Passenger do
-  it { should have_many :flight_passengers }
-  it { should have_many(:flights).through(:flight_passengers)}
+  describe "validations" do
+    it { should have_many :flight_passengers }
+    it { should have_many(:flights).through(:flight_passengers)}
+  end
+  describe "relationships" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+  end
 end
