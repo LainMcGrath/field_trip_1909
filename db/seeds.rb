72b236703c39a6621301b9e2f68c051d1951658d
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-13 11:15:59 -0700

    Add some seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+airline_1 = Airline.create(name: "United")
+flight_1 = Flight.create(number: "JX12", date: "02/24/2020", time: "1:30pm", arrival_city: "Chicago", departure_city: "Denver", airline_id: airline_1.id)
+flight_2 = Flight.create(number: "AG45", date: "02/24/2020", time: "6:00pm", arrival_city: "New York", departure_city: "Chicago", airline_id: airline_1.id)
+flight_3 = Flight.create(number: "GT6S", date: "03/01/2020", time: "2:00pm", arrival_city: "Denver", departure_city: "New York", airline_id: airline_1.id)
+
+passenger_1 = Passenger.create(name: "Caligula", age: 22)
+FlightPassenger.create(flight: flight_1, passenger: passenger_1)
